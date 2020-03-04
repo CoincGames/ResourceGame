6b8ed9db04f3d887d6a4893056621253ef3b7bb9
Scoutzknifez <codygaming@4alexanders.com>
2020-03-04 11:55:00 -0800

    Made the crosshair become disabled when the inv menu is open.

--- ResourceGame/Assets/Scripts/Player/PlayerInventory.cs
+++ ResourceGame/Assets/Scripts/Player/PlayerInventory.cs
@@ -10,6 +10,10 @@ public class PlayerInventory : MonoBehaviour
     [Tooltip("The menu to close on the inventory press key.")]
     private GameObject inventoryMenu;
 
+    [SerializeField]
+    [Tooltip("The crosshair view to open when the menu gets closed.")]
+    private GameObject crosshair;
+
     public Dictionary<Resource.ResourceType, int> invMap = new Dictionary<Resource.ResourceType, int>();
 
     private void Update()
@@ -21,9 +25,11 @@ public class PlayerInventory : MonoBehaviour
             if (!inventoryMenu.activeSelf)
             {
                 inventoryMenu.SetActive(true);
+                crosshair.SetActive(false);
             } else
             {
                 inventoryMenu.SetActive(false);
+                crosshair.SetActive(true);
             }
         }
     }
