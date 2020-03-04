1a66d513dbe062d0726513ffaec462297112cbca
Scoutzknifez <codygaming@4alexanders.com>
2020-03-04 13:58:59 -0800

    Began work on proc gen of maps.

---
+++ ResourceGame/Assets/Scripts/MapGen/MapGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [Range(1, 100)]
+    public int mapWidth;
+    [Range(1, 100)]
+    public int mapHeight;
+    [Range(.15f, 10f)]
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
