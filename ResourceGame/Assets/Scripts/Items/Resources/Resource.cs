50aebc4b2b50ad05552e693488de876e636a7f3f
Scoutzknifez <codygaming@4alexanders.com>
2020-06-17 22:59:22 -0700

    Added a suggestion on how to deal with item stacks...

--- ResourceGame/Assets/Scripts/Items/Resources/Resource.cs
+++ ResourceGame/Assets/Scripts/Items/Resources/Resource.cs
@@ -4,6 +4,7 @@ public class Resource : Item
 {
     public Resource()
     {
+        // Maybe have a way to do item stacks instead... very minecraft like
         amount = 1;
         maxStackSize = 100;
         dropped = false;
