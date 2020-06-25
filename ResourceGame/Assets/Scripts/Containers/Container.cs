04a93d3828d470f4a33e3318ee31034be6b4f305
Scoutzknifez <codygaming@4alexanders.com>
2020-06-25 01:22:40 -0700

    Added more ways to handle inventory functions

--- ResourceGame/Assets/Scripts/Containers/Container.cs
+++ ResourceGame/Assets/Scripts/Containers/Container.cs
@@ -4,30 +4,76 @@ using UnityEngine;
 
 public class Container : IEnumerable<ItemStack>
 {
-    private int size = 0;
+    private int maxSize = 0;
     private Dictionary<int, ItemStack> inventoryMap = new Dictionary<int, ItemStack>();
 
-    public Container(int size)
+    public Container(int maxSize)
     {
-        this.size = size;
+        this.maxSize = maxSize;
     }
 
-    public bool TryAddItem(ItemStack item)
+    public void TryAddItem(ItemStack item)
     {
-        // TODO New code here
-        // Check max size
-        // containedItems.Add(item);
-        return true;
+        if (inventoryMap.Count >= getMaxSize())
+            return; // TODO send a thing back, let them know inventory is full
+
+        inventoryMap.Add(inventoryMap.Count, item);
+
+        refresh();
+    }
+
+    public void SwapSlots(int slotFrom, int slotTo)
+    {
+        ItemStack fromItemStack;
+        inventoryMap.TryGetValue(slotFrom, out fromItemStack);
+
+        if (fromItemStack == null)
+            return;
+
+        ItemStack toItemStack;
+        inventoryMap.TryGetValue(slotTo, out toItemStack);
+
+        inventoryMap[slotTo] = fromItemStack;
+        inventoryMap[slotFrom] = toItemStack;
+
+        refresh();
+    }
+
+    public void refresh()
+    {
+        clearNulls();
+    }
+
+    public void clearNulls()
+    {
+        for(int i = 0; i < maxSize; i++)
+        {
+            Dictionary<int, ItemStack>.KeyCollection keys = inventoryMap.Keys;
+
+            foreach(int key in keys)
+            {
+                ItemStack checkingItemStack;
+                inventoryMap.TryGetValue(key, out checkingItemStack);
+
+                if (checkingItemStack == null)
+                    inventoryMap.Remove(key);
+            }
+        }
     }
 
     public int getSize()
     {
-        return size;
+        return inventoryMap.Count;
+    }
+
+    public int getMaxSize()
+    {
+        return maxSize;
     }
 
     public IEnumerator GetEnumerator()
     {
-        for (int index = 0; index < size; index++)
+        for (int index = 0; index < maxSize; index++)
         {
             yield return inventoryMap[index];
         }
@@ -35,7 +81,7 @@ public class Container : IEnumerable<ItemStack>
 
     IEnumerator<ItemStack> IEnumerable<ItemStack>.GetEnumerator()
     {
-        for (int index = 0; index < size; index++)
+        for (int index = 0; index < maxSize; index++)
         {
             yield return inventoryMap[index];
         }
