70e74bfe85b59c98dc4204a397a6f155f7f5f1f0
Scoutzknifez <codygaming@4alexanders.com>
2020-07-06 23:21:30 -0700

    Added an item factory game object to the scene

---
+++ ResourceGame/Assets/Scripts/Items/ItemFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFactory : MonoBehaviour
+{
+    [Header("Item Stack Dictionary")]
+    public ItemStackEntry[] itemStackEntry;
+
+    [System.Serializable]
+    public struct ItemStackEntry
+    {
+        public string name;
+        public GameObject itemStack;
+    }
+
+
+}
