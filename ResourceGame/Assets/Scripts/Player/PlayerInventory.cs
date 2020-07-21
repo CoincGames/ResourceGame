aae520b1254640ec06f4fa7db02fc9f8fcb80472
Scoutzknifez <codygaming@4alexanders.com>
2020-07-21 12:32:58 -0700

    Adding some sway to weapons when moving your aim point

--- ResourceGame/Assets/Scripts/Player/PlayerInventory.cs
+++ ResourceGame/Assets/Scripts/Player/PlayerInventory.cs
@@ -18,19 +18,51 @@ public class PlayerInventory : MonoBehaviour
     private GameObject crosshair = null;
 
     // TEMP
-    public ItemType itemInHand = ItemType.NULL;
-
-    // TEMP
     public Text inHandDisplay;
 
     public Container inventory;
 
+    public PlayerMovement playerMovement;
+    public GameObject FPSCamera;
+    public GameObject itemInHand;
+    public Vector3 ItemOffset;
+    public float ItemInHandDistanceFromCamera;
+
+    [Header("Sway Left/Right Constants")]
+    public float swayAmount;
+    public float maxSwayAmount;
+    public float smoothAmount;
+
+    [Header("Sway Up/Down Constants")]
+    public float rotationSwayAmount;
+    public float maxRotationSwayAmount;
+    public float rotationSmoothAmount;
+
+    [Header("Rotation Axis To Apply")]
+    public bool rotationX;
+    public bool rotationY;
+    public bool rotationZ;
+
+    /* Privates */
+    private Vector3 initPos;
+    private Quaternion initRot;
+
     private void Start()
     {
         inventory = new Container(9, true, inventoryOwner);
+
+        initPos = itemInHand.transform.localPosition;
+        initRot = itemInHand.transform.localRotation;
     }
 
     private void Update()
+    {
+        updateMouse();
+        updateGUI();
+        updateItemInHandLocation();
+    }
+
+    private void updateMouse()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
@@ -39,7 +71,53 @@ public class PlayerInventory : MonoBehaviour
             inventoryMenu.SetActive(!inventoryMenu.activeSelf);
             crosshair.SetActive(!crosshair.activeSelf);
         }
+    }
 
-        inHandDisplay.text = itemInHand.ToString();
+    private void updateGUI()
+    {
+        inHandDisplay.text = itemInHand.GetComponent<ItemStack>().name;
+    }
+
+    private void updateItemInHandLocation()
+    {
+        Vector3 offset = itemInHand.transform.rotation * ItemOffset;
+        itemInHand.transform.position = FPSCamera.transform.position + offset + FPSCamera.transform.forward * ItemInHandDistanceFromCamera;
+
+        // Sway
+        float movementX = -Input.GetAxis("Mouse X") * swayAmount;
+        float movementY = -Input.GetAxis("Mouse Y") * swayAmount;
+
+        movementX = Mathf.Clamp(movementX, -maxSwayAmount, maxSwayAmount);
+        movementY = Mathf.Clamp(movementY, -maxSwayAmount, maxSwayAmount);
+
+        Vector3 finalPos = new Vector3(movementX, movementY, 0);
+        itemInHand.transform.localPosition = Vector3.Lerp(itemInHand.transform.localPosition,initPos + finalPos, Time.deltaTime * smoothAmount);
+
+        // Rotation
+        float tiltY = -Input.GetAxis("Mouse X") * rotationSwayAmount;
+        float tiltX = -Input.GetAxis("Mouse Y") * rotationSwayAmount;
+
+        tiltY = Mathf.Clamp(tiltY, -maxRotationSwayAmount, maxRotationSwayAmount);
+        tiltX = Mathf.Clamp(tiltX, -maxRotationSwayAmount, maxRotationSwayAmount);
+
+        Quaternion finalRotPos = Quaternion.Euler(
+            new Vector3(
+                rotationX ? -tiltX : 0f,
+                rotationY ? tiltY : 0f,
+                rotationZ ? tiltY : 0f
+            )
+        );
+
+        itemInHand.transform.localRotation = Quaternion.Slerp(itemInHand.transform.localRotation, initRot * finalRotPos, Time.deltaTime * smoothAmount);
+
+        if (playerMovement.isMovingForwardBackward)
+        {
+            // Do little hops here
+        }
+
+        if (playerMovement.isMovingLeftRight)
+        {
+            // Do little sways here
+        }
     }
 }
